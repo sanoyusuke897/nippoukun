3a80b6f7fbc551ed684a6191c8f4034a0c52de16
佐野祐介 <yusukekure@sanoyuusukenoMacBook-Pro.local>
2023-01-12 10:03:25 +0900

    ログインページ修正

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -57,11 +57,11 @@
                                     {{ __('ログイン') }}
                                 </button>
 
-                                @if (Route::has('password.request'))
+                                {{-- @if (Route::has('password.request'))
                                     <a class="btn btn-link" href="{{ route('password.request') }}">
                                         {{ __('パスワードをお忘れですか?') }}
                                     </a>
-                                @endif
+                                @endif --}}
                             </div>
                         </div>
                     </form>
